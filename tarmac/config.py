bb171ce0d577b019e6c73e40a208b66a4b6643f4
Paul Hummer <paul@eventuallyanyway.com>
2009-02-13 00:30:23 -0700

    Fixed some issues with config

--- tarmac/config.py
+++ tarmac/config.py
@@ -2,21 +2,26 @@
 from ConfigParser import SafeConfigParser as ConfigParser
 import os
 
-
-CONFIG_DIR = os,path.expanduser('~/.config/tarmac')
+CONFIG_DIR = os.path.expanduser('~/.config/tarmac')
+CONFIG_FILE = os.path.join(CONFIG_DIR, 'config')
 
 def create_config_dirs():
     '''Create the configuration directory if it doesn't exist.'''
-    if not os.path.exists(os.path.expanduser('~/.config'):
-        os.mkdir(os.path.expanduser('~/.config')
-    if not os.path.exists(os.path.expanduser('~/.config/tarmac'):
-        os.mkdir(os.path.expanduser('~/.config/tarmac')
-    if not os.path.exists(os.path.expanduser('~/.config/tarmac/config'):
-        os.mkdir(os.path.expanduser('~/.config/tarmac/config')
+    if not os.path.exists(os.path.expanduser('~/.config')):
+        os.mkdir(os.path.expanduser('~/.config'))
+    if not os.path.exists(os.path.expanduser('~/.config/tarmac')):
+        os.mkdir(os.path.expanduser('~/.config/tarmac'))
+    if not os.path.exists(os.path.expanduser('~/.config/tarmac/cachedir')):
+        os.mkdir(os.path.expanduser('~/.config/tarmac/cachedir'))
 
 def get_config():
     '''Get the Configuration object.'''
     config = ConfigParser()
-    config.read([os.path.join(CONFIG_DIR, 'config')])
+    config.read([CONFIG_FILE])
+    try:
+        config.write(open(CONFIG_FILE, 'wb'))
+    except IOError:
+        create_config_dirs()
+        config.write(open(CONFIG_FILE, 'wb'))
     return config
 
