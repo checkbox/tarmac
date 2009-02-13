8a0d29f0bd3c5b3d1bc1d983f757d4a4b85ca993
Paul Hummer <paul@eventuallyanyway.com>
2009-02-12 23:29:34 -0700

    Added preliminary landings script, exploring what all I'll need for the rest of the process

---
+++ landings.py
@@ -0,0 +1,21 @@
+import os
+
+from launchpadlib.launchpad import Credentials, Launchpad, EDGE_SERVICE_ROOT
+
+
+LOGIN_CREDENTIALS = os.path.abspath('~/.config/tarmac/login-credentials')
+CACHEDIR = os.path.abspath('~/.config/tarmac/cache')
+
+if not os.path.exists(CACHEDIR):
+    os.mkdir(CACHEDIR)
+
+if os.path.exists(LOGIN_CREDENTIALS):
+    credentials = Credentials()
+    credentials.load(open(LOGIN_CREDENTIALS))
+    launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT, CACHEDIR)
+else:
+    launchpad = Launchpad.get_token_and_login('Tarmac',
+        STAGING_SERVICE_ROOT, cachedir)
+    launchpad.credentials.save(file(LOGIN_CREDENTIALS, 'w'))
+
+
