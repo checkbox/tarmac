cd061d2477ba54bd6c1936603d7a684d74449088
Paul Hummer <paul@eventuallyanyway.com>
2009-02-16 09:07:25 +0100

    Committing in the middle of my work...

--- lander.py
+++ lander.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
+from bzrlib import branch
+from bzrlib.plugin import load_plugins
 from launchpadlib.launchpad import Launchpad
 
+load_plugins()
+
+cachedir = '/tmp'
+
 launchpad = Launchpad.get_token_and_login('Tarmac',
-    STAGING_SERVICE_ROOT, cachedir)
+    'https://api.launchpad.dev/beta/', cachedir)
 #launchpad.credentials.save(file(LOGIN_CREDENTIALS, 'w'))
 
-project = launchpad.projects['entertainer']
-trunk = entertainer.development_focus.branch
+project = launchpad.projects['gnome-terminal']
+trunk_entry = project.development_focus.branch
 
-
-candidates = [entry for entry in trunk['landing_candidates'].entries
-                    if entry.queue_status == 'Approved']
+candidates = []
+for entry in trunk_entry.landing_candidates:
+    if entry.queue_status == u'Approved':
+        candidates.append(entry)
 
 for candidate in candidates:
-    # Branch trunk & build
-    # Merge
-    # Run Hooks
-    # Commit
-    # Push
+    print 'Branching %s to %s' % (candidate.target_branch.bzr_identity,
+        '/tmp/123456')
+    trunk = branch.Branch.open(candidate.target_branch.bzr_identity)
+    source = branch.Branch.open(candidate.source_branch.bzr_identity)
+    trunk_checkout = trunk.create_checkout('/tmp/1234561')
+
+    #trunk_tree.merge(candidate.source_branch.bzr_identity)
+    # TODO: Add hook code.
+    #trunk_tree.commit(candidates.all_comments.entries[0])
+    #trunk.push(trunk_remote)
 
--- landings.py
+++ landings.py
@@ -2,20 +2,23 @@ import os
 
 from launchpadlib.launchpad import Credentials, Launchpad, EDGE_SERVICE_ROOT
 
+from tarmac import config
 
-LOGIN_CREDENTIALS = os.path.abspath('~/.config/tarmac/login-credentials')
-CACHEDIR = os.path.abspath('~/.config/tarmac/cache')
+configuration = config.get_config()
 
-if not os.path.exists(CACHEDIR):
-    os.mkdir(CACHEDIR)
+#LOGIN_CREDENTIALS = os.path.abspath('~/.config/tarmac/login-credentials')
+#CACHEDIR = os.path.abspath('~/.config/tarmac/cache')
 
-if os.path.exists(LOGIN_CREDENTIALS):
-    credentials = Credentials()
-    credentials.load(open(LOGIN_CREDENTIALS))
-    launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT, CACHEDIR)
-else:
-    launchpad = Launchpad.get_token_and_login('Tarmac',
-        STAGING_SERVICE_ROOT, cachedir)
-    launchpad.credentials.save(file(LOGIN_CREDENTIALS, 'w'))
+#if not os.path.exists(CACHEDIR):
+#    os.mkdir(CACHEDIR)
+
+#if os.path.exists(LOGIN_CREDENTIALS):
+#    credentials = Credentials()
+#    credentials.load(open(LOGIN_CREDENTIALS))
+#    launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT, CACHEDIR)
+#else:
+#    launchpad = Launchpad.get_token_and_login('Tarmac',
+#        STAGING_SERVICE_ROOT, cachedir)
+#    launchpad.credentials.save(file(LOGIN_CREDENTIALS, 'w'))
 
 
