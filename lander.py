87156769b43a8643b6d900a5e0a9457799649ce3
Paul Hummer <paul@eventuallyanyway.com>
2009-02-19 14:06:36 +0100

    Added cache dir changes

--- lander.py
+++ lander.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 
+import os
+
 from bzrlib import branch
 from bzrlib.plugin import load_plugins
 from launchpadlib.launchpad import Launchpad
 
 load_plugins()
 
-cachedir = '/tmp'
+cachedir = '/tmp/tarmac-cache-%(pid)s' % {'pid': os.getpid()}
+print 'Caching to %(cachedir)s' % {'cachedir': cachedir}
 
 launchpad = Launchpad.get_token_and_login('Tarmac',
     'https://api.launchpad.dev/beta/', cachedir)
 #launchpad.credentials.save(file(LOGIN_CREDENTIALS, 'w'))
 
-project = launchpad.projects['gnome-terminal']
+project = launchpad.projects['loggerhead']
+import pdb; pdb.set_trace()
 trunk_entry = project.development_focus.branch
 
 candidates = []
 for entry in trunk_entry.landing_candidates:
+    print entry.source_branch.display_name
     if entry.queue_status == u'Approved':
+        print '      will land.'
         candidates.append(entry)
 
 for candidate in candidates:
     print 'Branching %s to %s' % (candidate.target_branch.bzr_identity,
         '/tmp/123456')
-    trunk = branch.Branch.open(candidate.target_branch.bzr_identity)
-    source = branch.Branch.open(candidate.source_branch.bzr_identity)
-    trunk_checkout = trunk.create_checkout('/tmp/1234561')
+    #trunk = branch.Branch.open(candidate.target_branch.bzr_identity)
+    #source = branch.Branch.open(candidate.source_branch.bzr_identity)
+    #trunk_checkout = trunk.create_checkout('/tmp/1234561')
 
     #trunk_tree.merge(candidate.source_branch.bzr_identity)
     # TODO: Add hook code.
