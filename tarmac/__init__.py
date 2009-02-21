201ba887f1c1334384dfd8ee32028d184ccf9171
Paul Hummer <paul@eventuallyanyway.com>
2009-02-20 18:32:44 -0700

    Added versioning

--- tarmac/__init__.py
+++ tarmac/__init__.py
@@ -1,3 +1,6 @@
 # Copyright (c) 2009 - Paul Hummer
 '''Tarmac - A tool for controlling the landing of Launchpad branches.'''
 
+__version__ = u'0.1'
+__copyright__ = u'Paul Hummer <paul@eventuallyanyway.com>'
+
