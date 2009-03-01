5691cde744656ce7cbc1416d94ec3329664e399a
Paul Hummer <paul@eventuallyanyway.com>
2009-03-01 22:28:18 +1300

    Made the main script a callable class

--- tarmac/bin.py
+++ tarmac/bin.py
@@ -15,59 +15,63 @@ load_plugins()
 DEV_SERVICE_ROOT = 'https://api.launchpad.dev/beta/'
 
 
-def main(*args, **kwargs):
+class main:
     '''Tarmac script.'''
-    configuration = TarmacConfig()
 
-    try:
-        launchpad = get_launchpad_object(configuration)
-    except HTTPError:
-        print (
-            'Oops!  It appears that the OAuth token is invalid.  Please '
-            'delete %(credential_file)s and re-authenticate.' %
-                {'credential_file': configuration.CREDENTIALS})
-        sys.exit()
+    def __init__(self, *args, **kwargs):
+        try:
+            self.project = launchpad.projects[args[1]]
+        except IndexError:
+            # This code is merely a placeholder until I can get proper argument
+            # handling, at which point this should print usage information.
+            print (
+                'Oops!  You\'ve forgotten to specify a project to land '
+                'branches for.  Please specify your project as the first '
+                'argument to tarmac-lander.')
+            sys.exit()
 
-    try:
-        project = launchpad.projects[args[1]]
-    except IndexError:
-        # This code is merely a placeholder until I can get proper argument
-        # handling, at which point this should print usage information.
-        print (
-            'Oops!  You\'ve forgotten to specify a project to land branches '
-            'for.  Please specify your project as the first argument to '
-            'tarmac-lander.')
-        sys.exit()
+    def __call__(self):
+        configuration = TarmacConfig()
 
-    try:
-        trunk = project.development_focus.branch
-    except AttributeError:
-        print (
-            'Oops!  It looks like you\'ve forgotten to specify a development '
-            'focus branch.  Please link your "trunk" branch to the trunk '
-            'development focus.')
-        sys.exit()
+        try:
+            launchpad = get_launchpad_object(configuration)
+        except HTTPError:
+            print (
+                'Oops!  It appears that the OAuth token is invalid.  Please '
+                'delete %(credential_file)s and re-authenticate.' %
+                    {'credential_file': configuration.CREDENTIALS})
+            sys.exit()
 
-    candidates = [entry for entry in trunk.landing_candidates
-                    if entry.queue_status == u'Approved']
+        project = launchpad.projects[self.project]
+        try:
+            trunk = project.development_focus.branch
+        except AttributeError:
+            print (
+                'Oops!  It looks like you\'ve forgotten to specify a '
+                'development focus branch.  Please link your "trunk" branch '
+                'to the trunk development focus.')
+            sys.exit()
 
-    for candidate in candidates:
-        temp_dir = '/tmp/merge-%(source)s-%(pid)s' % {
-            'source': candidate.source_branch.name,
-            'pid': os.getpid()
-            }
-        os.mkdir(temp_dir)
+        candidates = [entry for entry in trunk.landing_candidates
+                        if entry.queue_status == u'Approved']
 
-        accelerator, target_branch = bzrdir.BzrDir.open_tree_or_branch(
-            candidate.target_branch.bzr_identity)
-        target_tree = target_branch.create_checkout(
-            temp_dir, None, True, accelerator)
-        source_branch = branch.Branch.open(
-            candidate.source_branch.bzr_identity)
+        for candidate in candidates:
+            temp_dir = '/tmp/merge-%(source)s-%(pid)s' % {
+                'source': candidate.source_branch.name,
+                'pid': os.getpid()
+                }
+            os.mkdir(temp_dir)
 
-        target_tree.merge_from_branch(source_branch)
-        # TODO: Add hook code.
-        target_tree.commit(candidate.all_comments[0].message_body)
+            accelerator, target_branch = bzrdir.BzrDir.open_tree_or_branch(
+                candidate.target_branch.bzr_identity)
+            target_tree = target_branch.create_checkout(
+                temp_dir, None, True, accelerator)
+            source_branch = branch.Branch.open(
+                candidate.source_branch.bzr_identity)
+
+            target_tree.merge_from_branch(source_branch)
+            # TODO: Add hook code.
+            target_tree.commit(candidate.all_comments[0].message_body)
 
 
 
