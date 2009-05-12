707fa733eb40c1d1de14d8a57da815b64d781621
Paul Hummer <paul@eventuallyanyway.com>
2009-05-11 20:07:02 -0600

    Added hook code to the bzr plugin

--- bzrplugin/__init__.py
+++ bzrplugin/__init__.py
@@ -1,1 +1,12 @@
 '''Bazaar Plugin for Tarmac.'''
+
+from bzrlib.lazy_import import lazy_import
+lazy_import(globals(), '''
+from bzrlib.hooks import known_hooks
+
+''')
+
+known_hooks.register_lazy(
+    ('tarmac.hooks.TarmacHooks', 'TarmacLander.hooks'),
+    'tarmac.hooks', 'TarmacHooks')
+
