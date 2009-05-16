70af996b9ea54f7d4cfc868a8f614e5c8929d397
Paul Hummer <paul@eventuallyanyway.com>
2009-05-15 20:53:16 -0600

    Testing against STAGING

--- tarmac/utils.py
+++ tarmac/utils.py
@@ -2,19 +2,20 @@
 '''Utilities for Tarmac, mostly dealing with Launchpad.'''
 import os
 
-from launchpadlib.launchpad import Credentials, Launchpad, EDGE_SERVICE_ROOT
+from launchpadlib.launchpad import (Credentials, Launchpad, EDGE_SERVICE_ROOT,
+    STAGING_SERVICE_ROOT)
 
 
 def get_launchpad_object(config):
     '''Return an autheticated launchpad API object.'''
     if not os.path.exists(config.CREDENTIALS):
         launchpad = Launchpad.get_token_and_login('Tarmac',
-            EDGE_SERVICE_ROOT, config.CACHEDIR)
+            STAGING_SERVICE_ROOT, config.CACHEDIR)
         launchpad.credentials.save(file(config.CREDENTIALS, 'w'))
     else:
         credentials = Credentials()
         credentials.load(open(config.CREDENTIALS))
-        launchpad = Launchpad(credentials, EDGE_SERVICE_ROOT,
+        launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT,
             config.CACHEDIR)
 
     return launchpad
