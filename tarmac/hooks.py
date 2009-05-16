b89fd90bdee90b71df313d2cbd54138aeeb2a304
Paul Hummer <paul@eventuallyanyway.com>
2009-05-15 19:59:34 -0600

    Fleshed out hooks and hookpoints for Tarmac

--- tarmac/hooks.py
+++ tarmac/hooks.py
@@ -4,21 +4,35 @@
 from bzrlib import hooks
 
 
-class TarmacHooks(hooks.Hooks):
+class TarmacHookPoint(hooks.HookPoint):
+    '''A special HookPoint for Tarmac.
+
+    This HookPoint implements a fire method that the bzrlib.hooks.HookPoint
+    doesn't have.  If this turns out to be helpful, than a patch to Bazaar
+    should be made to implement it in Bazaar.
+    '''
+
+    def fire(self, *args, **kwargs):
+        '''Fire all registered hooks for self.'''
+        for hook in self:
+            hook(*args, **kwargs)
+
+
+class TarmacHookRegistry(hooks.Hooks):
     '''Hooks for Tarmac.'''
 
     def __init__(self):
         hooks.Hooks.__init__(self)
-        self.create_hook(hooks.HookPoint('pre_tarmac_commit',
+
+        self.create_hook(TarmacHookPoint('pre_tarmac_commit',
             'Called right after Tarmac checks out and merges in a new '
             'branch, but before committing.',
             (1, 14, 0), False))
 
+        self.create_hook(TarmacHookPoint('post_tarmac_commit',
+            'Called right after Tarmac commits the merged revision',
+            (1, 14, 0), False))
 
-        self.create_hook(hooks.HookPoint('merge_request_body',
-            "Called with a MergeRequestBodyParams when a body is needed for"
-            " a merge request.  Callbacks must return a body.  If more"
-            " than one callback is registered, the output of one callback is"
-            " provided to the next.", (1, 15, 0), False))
 
+tarmac_hooks = TarmacHookRegistry()
 
