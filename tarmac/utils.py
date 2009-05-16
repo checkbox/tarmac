63eaeb114cee1ccc3cb42ada3ef6d80937971080
Paul Hummer <paul@eventuallyanyway.com>
2009-05-15 23:26:11 -0600

    Added testing version of the launchpad object

--- tarmac/utils.py
+++ tarmac/utils.py
@@ -6,16 +6,21 @@ from launchpadlib.launchpad import (Credentials, Launchpad, EDGE_SERVICE_ROOT,
     STAGING_SERVICE_ROOT)
 
 
-def get_launchpad_object(config):
+def get_launchpad_object(config, staging=False):
     '''Return an autheticated launchpad API object.'''
+    if staging:
+        SERVICE_ROOT = STAGING_SERVICE_ROOT
+    else:
+        SERVICE_ROOT = EDGE_SERVICE_ROOT
+
     if not os.path.exists(config.CREDENTIALS):
         launchpad = Launchpad.get_token_and_login('Tarmac',
-            STAGING_SERVICE_ROOT, config.CACHEDIR)
+            SERVICE_ROOT, config.CACHEDIR)
         launchpad.credentials.save(file(config.CREDENTIALS, 'w'))
     else:
         credentials = Credentials()
         credentials.load(open(config.CREDENTIALS))
-        launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT,
+        launchpad = Launchpad(credentials, SERVICE_ROOT,
             config.CACHEDIR)
 
     return launchpad
