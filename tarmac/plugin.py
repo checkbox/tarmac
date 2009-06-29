9b89670d139d4a2245a9e0744af416c5c80a42ed
Paul Hummer <paul@eventuallyanyway.com>
2009-06-28 22:22:00 -0600

    Fixed lint in tarmac.plugin

--- tarmac/plugin.py
+++ tarmac/plugin.py
@@ -1,4 +1,5 @@
 '''Plugin utilities for Tarmac.'''
+# pylint: disable-msg=W0122,W0612
 
 import imp
 import os
@@ -20,8 +21,8 @@ def load_plugins():
     plugin_names = set()
     for path in TARMAC_PLUGIN_PATHS:
         try:
-            for file in os.listdir(path):
-                full_path = os.path.join(path, file)
+            for _file in os.listdir(path):
+                full_path = os.path.join(path, _file)
                 if os.path.isdir(full_path):
                     for entry in package_entries:
                         if os.path.isfile(os.path.join(full_path, entry)):
@@ -31,23 +32,24 @@ def load_plugins():
                             continue
 
                 else:
-                    if file.startswith('.'):
+                    if _file.startswith('.'):
                         continue # Hidden file, should be ignored.
                     for suffix_info in imp.get_suffixes():
-                        if file.endswith(suffix_info[0]):
-                            file = file[:-len(suffix_info[0])]
-                            if suffix_info[2] == imp.C_EXTENSION and file.endswith('module'):
-                                file = file[:-len('module')]
+                        if _file.endswith(suffix_info[0]):
+                            _file = _file[:-len(suffix_info[0])]
+                            if (suffix_info[2] == imp.C_EXTENSION and
+                                                    _file.endswith('module')):
+                                _file = _file[:-len('module')]
                             break
                         else:
                             continue
 
-                if file == '__init__':
+                if _file == '__init__':
                     continue
-                elif getattr(_mod_plugins, file, None):
+                elif getattr(_mod_plugins, _file, None):
                     continue # Plugin is already loaded.
                 else:
-                    plugin_names.add(file)
+                    plugin_names.add(_file)
         except OSError: # Usually the dir does not exist
             continue
 
@@ -56,10 +58,4 @@ def load_plugins():
             exec 'import tarmac.plugins.%s' % name in {}
         except KeyboardInterrupt:
             raise
-
-def load_plugin_from_dir(directory):
-    '''Load a plugin from the dir.'''
-
-def load_plugin_from_file(file):
-    '''Load a plugin from a file.'''
 
