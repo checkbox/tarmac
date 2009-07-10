30a3b3c27019f089b3903fe97033bb09f782c229
Elliot Murphy <elliot@elliotmurphy.com>
2009-07-10 11:10:28 -0400

    Add GPL3 license headers to all the files and correct setup.py to say GPLv3 instead of AGPLv3.

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
-# Copyright 2009 Paul Hummer - See LICENSE
+# Copyright 2009 Paul Hummer
+#
+# This file is part of Tarmac.
+#
+# Tarmac is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by
+# the Free Software Foundation.
+#
+# Tarmac is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tarmac.  If not, see <http://www.gnu.org/licenses/>.
 '''Tarmac installation script.'''
 
 from distutils.core import setup
@@ -11,12 +26,12 @@ setup(
     version=__version__,
     description=u'Tarmac - The Launchpad Lander',
     url=u'http://edge.launchpad.net/tarmac',
-    license=u'AGPLv3',
+    license=u'GPLv3',
     package_dir={
         'bzrlib.plugins.tarmac': 'bzrplugin',
         'tarmac': 'tarmac'},
     packages=['bzrlib.plugins.tarmac', 'tarmac'],
-    scripts=['tarmac-lander'],
+    scripts=['tarmac-lander', 'tarmac-authenticate'],
     long_description='''
         Tarmac is a series of scripts to facilitate the landing of Bazaar
         branches in Launchpad (http://edge.launchpad.net).''',
