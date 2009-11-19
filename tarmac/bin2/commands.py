7675b5f4412222286437798eb0ec45d3043bc35b
Paul Hummer <paul@eventuallanyway.com>
2009-11-19 13:49:15 -0700

    Added CommandDispatch class

--- tarmac/bin2/commands.py
+++ tarmac/bin2/commands.py
@@ -1,4 +1,13 @@
 '''Command handling for Tarmac.'''
 
+class CommandDispatch():
+    '''Class for handling command dispatch.'''
+
+    def run(self):
+        '''Execute the command.'''
+
+
 def main():
     '''Main script handler.'''
+    dispatch = CommandDispatch()
+    dispatch.run()
