8f779a668096805f92ec0102480f7b4d477005f4
Paul Hummer <paul@eventuallanyway.com>
2009-11-23 11:48:24 -0700

    Added docs for TestTarmacConfig2

--- tarmac/tests/test_config.py
+++ tarmac/tests/test_config.py
@@ -8,14 +8,14 @@ class TestTarmacConfig2(unittest.TestCase):
     '''Testing for tarmac.config.TarmacConfig2.'''
 
     def test_CONFIG_HOME(self):
+        '''Return the default CONFIG_HOME.'''
         config = TarmacConfig2()
         self.assertEqual(
             config.CONFIG_HOME,
             os.path.expanduser('~/.config/tarmac'))
 
     def test_CONFIG_HOME_environment(self):
+        '''If TARMAC_CONFIG_HOME environment variable is set, use it.'''
         os.environ['TARMAC_CONFIG_HOME'] = '/'
         config = TarmacConfig2()
-        self.assertEqual(
-            config.CONFIG_HOME,
-            '/')
+        self.assertEqual(config.CONFIG_HOME, '/')
