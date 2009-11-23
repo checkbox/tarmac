17f69c6796d18740975b4e0d9803134439bab1d0
Paul Hummer <paul@eventuallanyway.com>
2009-11-23 10:46:14 -0700

    Fixed the TestAuthCommand.test_invoke script to not print to stdout and also actually assert something

--- tarmac/tests/test_commands.py
+++ tarmac/tests/test_commands.py
@@ -1,4 +1,6 @@
 '''Tests for tarmac.bin2.commands.py.'''
+from cStringIO import StringIO
+import sys
 import unittest
 
 from tarmac.bin2.commands import AuthCommand, Command, CommandRegistry
@@ -62,5 +64,12 @@ class TestAuthCommand(unittest.TestCase):
     '''Test for tarmac.bin2.command.AuthCommand.'''
 
     def test_invoke(self):
+        tmp_stdout = StringIO()
+        old_stdout = sys.stdout
+        sys.stdout = tmp_stdout
+
         command = AuthCommand()
         command.invoke()
+        self.assertEqual(tmp_stdout.getvalue(), 'authenticated\n')
+
+        sys.stdout = old_stdout
