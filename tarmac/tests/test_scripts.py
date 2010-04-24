18d5e1511339f8c35da15068282b650b94e766a0
Paul Hummer <paul@eventuallyanyway.com>
2010-04-24 03:33:10 +0000

    Removed tarmac.bin

--- tarmac/tests/test_scripts.py
+++ tarmac/tests/test_scripts.py
@@ -22,49 +22,7 @@ import commands
 from optparse import OptionParser
 import sys
 
-from tarmac.bin import TarmacLander, TarmacScript
 from tarmac.tests import TarmacTestCase
-
-
-class TestTarmacScript(TarmacTestCase):
-    '''Tests for tarmac.bin.TarmacScript.'''
-
-    class TarmacDummyScript(TarmacScript):
-        '''A dummy Tarmac script for testability.'''
-        def _create_option_parser(self):
-            return OptionParser()
-
-    def test_create_option_parser_not_implemented(self):
-        '''Test that the _create_config_parser method raises NotImplemented.'''
-        self.assertRaises(NotImplementedError, TarmacScript, test_mode=True)
-
-    def test_dummy_script_option_parser(self):
-        '''Test that _create_config_parser is implemented in TarmacDummyScript.
-        '''
-        sys.argv = ['']
-        script = self.TarmacDummyScript(test_mode=True)
-        self.assertTrue(isinstance(script, self.TarmacDummyScript))
-
-
-class TestTarmacLander(TarmacTestCase):
-    '''Tests for TarmacLander.'''
-
-    def test_lander_dry_run(self):
-        '''Test that setting --dry-run sets the dry_run property.'''
-        sys.argv = ['', 'foo', '--dry-run']
-        script = TarmacLander(test_mode=True)
-        self.assertTrue(script.dry_run)
-
-    def test_lander_project(self):
-        '''Test that the project argument gets handled properly.'''
-        sys.argv = ['', 'foo']
-        script = TarmacLander(test_mode=True)
-        self.assertEqual(script.project, u'foo')
-
-    def test_test_mode(self):
-        '''Test that test_mode is set correctly.'''
-        script = TarmacLander(test_mode=True)
-        self.assertTrue(script.test_mode)
 
 
 class TestTarmacScript(TarmacTestCase):
