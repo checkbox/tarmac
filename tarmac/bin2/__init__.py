d0937883767cb36ad1b8ac7a854cda4c14423941
Paul Hummer <paul@eventuallyanyway.com>
2010-04-24 03:21:32 +0000

    Starting final cleanup for Tarmac 0.3 release

--- tarmac/bin2/__init__.py
+++ tarmac/bin2/__init__.py
@@ -1,7 +1,7 @@
 '''Script handling package for Tarmac.
 
 NOTE: This will replace tarmac.bin and provide all the functionalities that
-tarmac.bin currently provides. spec=tarmac-0.3
+tarmac.bin currently provides.
 '''
 def main():
     '''Main script handler.'''
