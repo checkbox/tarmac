38d63826a813163eebd4c47ad5c90b49d20c9246
Gavin Panella <gavin@gromper.net>
2010-09-02 15:36:07 +0100

    Whittle down the test data created in TestVotes.setUp() to just what's needed.

--- tarmac/plugins/tests/test_votes.py
+++ tarmac/plugins/tests/test_votes.py
@@ -17,28 +17,11 @@ class TestVotes(TarmacTestCase):
     def setUp(self):
         super(TestVotes, self).setUp()
         self.proposal = thing(
-            source_branch=thing(
-                owner=thing(display_name="Arthur Author", name="arthur"),
-                linked_bugs=[thing(id=1234), thing(id=5678)]),
-            commit_message="Awesome",
-            reviewer=thing(display_name="Randy Reviewer"),
             votes=[
-                thing(
-                    comment=thing(vote=u"Approve"),
-                    reviewer=thing(
-                        display_name="Virgil Voter", name="virgil")),
-                thing(
-                    comment=thing(vote=u"Approve"),
-                    reviewer=thing(
-                        display_name="Virginia Voter", name="virginia")),
-                thing(
-                    comment=thing(vote=u"Abstain"),
-                    reviewer=thing(
-                        display_name="Virginia Voter", name="virginia")),
-                thing(
-                    comment=thing(vote=u"Needs Information"),
-                    reviewer=thing(
-                        display_name="Virginia Voter", name="virginia")),
+                thing(comment=thing(vote=u"Approve")),
+                thing(comment=thing(vote=u"Approve")),
+                thing(comment=thing(vote=u"Abstain")),
+                thing(comment=thing(vote=u"Needs Information")),
                 ])
         self.votes = Votes()
 
