383d7472eb19ae31610d23c09214910a54e5c54b
Paul Hummer <paul@eventuallyanyway.com>
2010-10-25 14:20:18 -0600

    Fixed the parameter thing

--- tarmac/bin/registry.py
+++ tarmac/bin/registry.py
@@ -19,9 +19,11 @@ class CommandRegistry(object):
 
     def _get_command(self, command, name):
         '''Return the command.'''
-        if name.startswith('--'):
+        if name.startswith('-'):
             if name == '--help':
                 name = 'help'
+            else:
+                return
         _command = None
         try:
             _command = self._registry[name]
